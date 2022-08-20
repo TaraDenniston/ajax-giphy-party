bfdfb51b6e71cefa2d3e55e19a9528bc81e5b21e
Tara Denniston <taradenniston@gmail.com>
2022-08-20 13:18:26 -0400

    added remove button

--- app.js
+++ app.js
@@ -13,14 +13,12 @@ async function getGif(searchTerm) {
   try {
     // Send request
     const response = await axios.get(endpoint, {params: options});
-    console.log(response); //************************************************ */
 
     // Extract url for random GIF
     const array = response.data.data;
     const idx = Math.floor(Math.random() * array.length)
 
     // return array[idx].embed_url;
-    console.log(`getGif() url: ${array[idx].images.original.url}`); // *****************
     appendGif(array[idx].images.original.url);
 
   } catch(error) {
@@ -31,16 +29,15 @@ async function getGif(searchTerm) {
   }  
 }
 
+const board = document.querySelector('#gif-board');
 const appendGif = (url) => {
-  console.log(`appendGif() url: ${url}`); // *********************************
-  const board = document.querySelector('#gif-board');
-  console.log(board); // *********************************
   const newGif = document.createElement('div');
+  newGif.className = "p-3";
   const img = document.createElement('img');
+  img.style = "max-height: 18rem;"
   img.src = url;
   newGif.append(img);
   board.append(newGif);
-  console.log(board); // *********************************
 }
 
 const form = document.querySelector('#search-form');
@@ -48,6 +45,13 @@ const input = document.querySelector('#search');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   getGif(input.value);
+  input.value = '';
+})
+
+const removeBtn = document.querySelector('#remove');
+removeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  board.innerHTML = '';
 })
 
 
